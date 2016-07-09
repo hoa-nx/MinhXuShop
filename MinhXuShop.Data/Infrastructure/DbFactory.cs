f97527c1fc91c5bd551a7ff9f9c94ccb63c2b383
thieu_nx <xuanhoa97@gmail.com>
2016-07-09 23:10:24 +0700

    add entity framwork code first for data

---
+++ MinhXuShop.Data/Infrastructure/DbFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhXuShop.Data.Infrastructure
+{
+    public class DbFactory : Disposable, IDbFactory
+    {
+        MinhXuShopDbContext dbContext;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public MinhXuShopDbContext Init()
+        {
+            return dbContext ?? (dbContext = new MinhXuShopDbContext());
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected override void DisposeCore()
+        {
+            if (dbContext != null)
+            {
+                dbContext.Dispose();
+            }
+        }
+
+    }
+
+}
---
+++ MinhXuShop.Data/Infrastructure/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhXuShop.Data.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly IDbFactory dbFactory;
+        private MinhXuShopDbContext dbContext;
+
+        public UnitOfWork( IDbFactory dbFactory)
+        {
+            this.dbFactory = dbFactory;
+        }
+        public MinhXuShopDbContext DbContext
+        {
+            get { return dbContext ?? (dbContext = new MinhXuShopDbContext()); }
+        }
+
+        public void Commit()
+        {
+            DbContext.SaveChanges();
+        }
+    }
+
+}
