2d6d2744fc7e5b7c3430bc652ffbd1734e3a6204
thieu_nx <xuanhoa97@gmail.com>
2016-07-11 00:50:32 +0700

    Commit more service

--- MinhXuShop.Service/PostService.cs
+++ MinhXuShop.Service/PostService.cs
@@ -16,6 +16,7 @@ namespace MinhXuShop.Service
         void Delete(int id);
         IEnumerable<Post> GetAll();
         IEnumerable<Post> GetAllPaging(int page , int pageZise , out int totalRow);
+        IEnumerable<Post> GetAllByCategoryPaging(int categoryId , int page, int pageZise, out int totalRow);
         Post GetById(int id);
         IEnumerable<Post> GetAllByTagPaging(string tag,int page, int pageZise, out int totalRow);
         void SaveChanges();
@@ -45,10 +46,15 @@ namespace MinhXuShop.Service
             return _postRepository.GetAll(new string[] { "PostCategory" });
         }
 
+        public IEnumerable<Post> GetAllByCategoryPaging(int categoryId, int page, int pageZise, out int totalRow)
+        {
+            return _postRepository.GetMultiPaging(x => x.Status && x.CategoryID == categoryId,out totalRow,page, pageZise,new string[] {"PostCategory" });
+        }
+
         public IEnumerable<Post> GetAllByTagPaging(string tag , int page, int pageZise, out int totalRow)
         {
             //TODO : Select all post by tag
-            return _postRepository.GetMultiPaging(x => x.Status, out totalRow, page, pageZise);
+            return _postRepository.GetAllByTag(tag, page, pageZise, out totalRow);
         }
 
         public IEnumerable<Post> GetAllPaging(int page, int pageZise, out int totalRow)
