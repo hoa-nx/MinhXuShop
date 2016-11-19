5638c728bfe1ea0013128ce218fe03472a5c7042
thieu_nx <xuanhoa97@gmail.com>
2016-11-19 14:38:54 +0700

    filter productcategory

---
+++ MinhXuShop.Web/app/shared/filters/statusFilter.js
@@ -0,0 +1,11 @@
+﻿(function (app) {
+    app.filter('statusFilter', function () {
+        return function (input) {
+            if (input == true) {
+                return 'kich hoat';
+            } else {
+                return 'khoa';
+            }
+        }
+    });
+})(angular.module('minhxushop.common'));
