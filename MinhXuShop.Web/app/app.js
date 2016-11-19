320c5b4bc06b8f7e41e6cc0709cc06da601bdfd1
thieu_nx <xuanhoa97@gmail.com>
2016-11-19 10:01:31 +0700

    add phan resource admin va router

--- MinhXuShop.Web/app/app.js
+++ MinhXuShop.Web/app/app.js
@@ -1,1 +1,14 @@
-﻿
+﻿/// <reference path="D:\Woking\Web\Asp.NetMVC\AngularJS-MVC\Git\MinhXuShop.Web\Assets/admin/libs/angular/angular.js" />
+
+(function () {
+    angular.module('minhxushop', ['minhxushop.common', 'minhxushop.products']).config(config);
+    config.$inject = ['$stateProvider', '$urlRouterProvider'];
+    function config($stateProvier, $urlRouterProvider) {
+        $stateProvier.state('home', {
+            url: "/admin",
+            templateUrl: "/app/components/home/homeView.html",
+            controller: "homeController"
+        });
+        $urlRouterProvider.otherwise('/admin');
+    }
+})();
---
+++ MinhXuShop.Web/app/shared/modules/minhxushop.common.js
@@ -0,0 +1,3 @@
+﻿(function () {
+    angular.module('minhxushop.common', ['ui.router'])
+})();
